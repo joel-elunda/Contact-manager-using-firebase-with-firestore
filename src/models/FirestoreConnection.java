39ac4c8cc9474eab274970494176d3b7d5f4a64f
Joël Elunda <joel.elunda.01@gmail.com>
2020-07-06 20:49:54 +0200

    Firestore connection & firestore queries files

---
+++ src/models/FirestoreConnection.java
@@ -0,0 +1,10 @@
+package models;
+
+public class FirestoreConnection {
+
+    private static final String path = "contact-firebase-backend";
+
+    public String db() {
+        return "";
+    }
+}
---
+++ src/models/FirestoreQueries.java
@@ -0,0 +1,18 @@
+package models;
+
+import java.util.ArrayList;
+
+public class FirestoreQueries {
+
+    public static boolean add(ArrayList data, String collection) {
+        return false;
+    }
+
+    public static boolean delete(String ID, String collection) {
+        return false;
+    }
+
+    public static boolean update(ArrayList data, String collection) {
+        return false;
+    }
+}
